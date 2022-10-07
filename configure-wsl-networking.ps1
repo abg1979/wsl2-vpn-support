1fb52e652fe33f7d46c596d4bafe073752a7ee03
Abhishek Gupta <abhisheg@adobe.com>
2022-10-07 15:37:42 +0530

    Using Get-HNSEndpoint

--- configure-wsl-networking.ps1
+++ configure-wsl-networking.ps1
@@ -1,24 +1,21 @@
 ########### Configuration Parameters
 
-$vpn_interface_desc = "PANGP Virtual Ethernet Adapter"
+$vpn_interface_desc = "PANGP"
 $wsl_interface_name = "vEthernet (WSL)"
-
-$config_default_wsl_guest = 1 # 0: False, 1: True
-$wsl_guest_list = @()
 
 $state_file = "$HOME\wsl-added-routes.txt"
 
 ########### End Configuration Parameters
 
-echo "===================="
-echo "= WSL2 VPN Support ="
-echo "===================="
+Write-Output "===================="
+Write-Output "= WSL2 VPN Support ="
+Write-Output "===================="
 
 # Load Previous rules from file
-echo "Checking for previous configuration ..."
+Write-Output "Checking for previous configuration ..."
 $previous_ips = [System.Collections.ArrayList]@()
 if ((Test-Path $state_file)) {
-    echo "Loading State"
+    Write-Output "Loading State"
     foreach ($item IN (Get-Content -Path $state_file)) {
         $arrayId = $previous_ips.Add($item.Trim())
     }
@@ -26,55 +23,42 @@ if ((Test-Path $state_file)) {
 
 
 # Check if VPN Gateway is UP
-echo "Checking VPN State ..."
+Write-Output "Checking VPN State ..."
 $vpn_state = (Get-NetAdapter | Where-Object {$_.InterfaceDescription -Match "$vpn_interface_desc"} | select -ExpandProperty Status)
 if ($vpn_state -eq "Up") {
-    echo "VPN is UP"
+    Write-Output "VPN is UP"
 
     # Get key metrics for the WSL Network Interface
-    echo "Determining WSL2 Interface parameters ..."
+    Write-Output "Determining WSL2 Interface parameters ..."
     $wsl_interface_index = (Get-NetAdapter -Name "$wsl_interface_name" | select -ExpandProperty ifIndex)
 
-    echo "Determining VPN Interface parameters ..."
+    Write-Output "Determining VPN Interface parameters ..."
     $vpn_interface_index = (Get-NetAdapter | Where-Object {$_.InterfaceDescription -Match "$vpn_interface_desc"} | select -ExpandProperty ifIndex)
     $vpn_interface_routemetric = (Get-NetRoute -InterfaceIndex $vpn_interface_index | select -ExpandProperty RouteMetric | Sort-Object -Unique | Select-Object -First 1) + 1
 
     # Get list of IPs for the WSL Guest(s)
-    echo "Determining IP Addresses of WSL2 Guest(s) ..."
-    $wsl_guest_ips = [System.Collections.ArrayList]@()
-    if ($config_default_wsl_guest -gt 0) {
-        $guest_ip = (wsl /bin/sh -c "ip route get 1.2.3.4 | cut -d ' ' -f7 | tr -d '\n'")
-        $arrayId = $wsl_guest_ips.Add($guest_ip.Trim())
-        $previous_ips.Remove($guest_ip.Trim())
-    }
-
-    foreach ($guest_name IN $wsl_guest_list) {
-        $guest_ip = (wsl --distribution $guest_name /bin/sh -c "ip route get 1.2.3.4 | cut -d ' ' -f7 | tr -d '\n'")
-        $arrayId = $wsl_guest_ips.Add($guest_ip.Trim())
-        $previous_ips.Remove($guest_ip.Trim())
-    }
-
-
+    Write-Output "Determining IP Addresses of WSL2 Guest(s) ..."
+    $wsl_guest_ips = (Get-HnsEndpoint | select -ExpandProperty IPAddress)
     # Create rules for each WSL guest
-    echo "Creating routes ..."
-    echo $wsl_guest_ips | Out-File -FilePath $state_file
+    Write-Output "Creating routes ..."
+    Write-Output $wsl_guest_ips | Out-File -FilePath $state_file
     foreach ($ip IN $wsl_guest_ips) {
-        echo "Creating route for $ip"
+        Write-Output "Creating route for $ip"
         route add $ip mask 255.255.255.255 $ip metric $vpn_interface_routemetric if $wsl_interface_index
     }
 } else {
-    echo "VPN is DOWN"
-    echo "" | Out-File -FilePath $state_file
+    Write-Output "VPN is DOWN"
+    Write-Output "" | Out-File -FilePath $state_file
 }
 
 
 # Clean up previous IPs
-echo "Performing cleanup ..."
+Write-Output "Performing cleanup ..."
 foreach ($ip IN $previous_ips) {
     if ($ip.Trim() -ne "") {
-        echo "Deleting route for $ip"
+        Write-Output "Deleting route for $ip"
         route delete $ip mask 255.255.255.255 $ip
     }
 }
 
-echo "Done"
+Write-Output "Done"
